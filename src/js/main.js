bbacddd1106656bf3d7478fda89ce4cff9ab5ed4
Svetlana <svetlanagreenfox@gmail.com>
2023-07-12 15:39:07 +0300

    Added map api

--- src/js/main.js
+++ src/js/main.js
@@ -26,68 +26,34 @@ phoneInputs.forEach((input) => {
 
 /* Yandex Map */
 
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+const LOCATION = {center: [30.338928, 59.94354], zoom: 16};
 
-// ymaps3.ready.then(() => {
-//     const {YMaps} = ymaps3;
-//     new YMaps('map', {
-//         		// Координаты центра карты.
-//         		// Порядок по умолчанию: «широта, долгота».
-//         		// Чтобы не определять координаты центра карты вручную,
-//         		// воспользуйтесь инструментом Определение координат.
-//         		center: [59.943543, 30.338928],
-//         		// Уровень масштабирования. Допустимые значения:
-//         		// от 0 (весь мир) до 19.
-//         		zoom: 16,
-//         	});
-//   });
+let map;
 
-// ymaps.ready(init);
-// function init(){
-// 	// Создание карты.
-// 	var map = new ymaps.Map('map', {
-// 		// Координаты центра карты.
-// 		// Порядок по умолчанию: «широта, долгота».
-// 		// Чтобы не определять координаты центра карты вручную,
-// 		// воспользуйтесь инструментом Определение координат.
-// 		center: [59.943543, 30.338928],
-// 		// Уровень масштабирования. Допустимые значения:
-// 		// от 0 (весь мир) до 19.
-// 		zoom: 16,
-// 	});
+main();
+async function main() {
+    await ymaps3.ready;
+    const {
+        YMap,
+        YMapDefaultSchemeLayer,
+        YMapControls,
+        YMapDefaultFeaturesLayer,
+        YMapMarker
+    } = ymaps3;
 
-// 	var myPlacemark = new ymaps.Placemark(
-// 		[59.943543, 30.338928],
-// 		{
-// 			balloonContent: `
-// 				<div class="balloon">
-// 					<div class="balloon__address">Наб. реки Фонтанки 10-15</div>
-// 					<div class="balloon__contacts">
-// 						<a href="tel:+78121234567">+8 (812) 123-45-67</a>
-// 					</div>
-// 				</div>
-// 			`,
-// 		},
-// 		{
-// 			iconLayout: 'default#image',
-// 			iconImageHref: './img/map/location-pin.svg',
-// 			iconImageSize: [40, 40],
-// 			iconImageOffset: [-20, -40],
-// 		}
-// 	);
+    const {YMapZoomControl} = await ymaps3.import('@yandex/ymaps3-controls@0.0.1');
 
-// 	map.controls.remove('geolocationControl'); // удаляем геолокацию
-// 	map.controls.remove('searchControl'); // удаляем поиск
-// 	map.controls.remove('trafficControl'); // удаляем контроль трафика
-// 	map.controls.remove('typeSelector'); // удаляем тип
+    map = new YMap(document.getElementById('map'), {location: LOCATION});
 
-// 	// map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-// 	// map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-// 	map.controls.remove('rulerControl'); // удаляем контрол правил
-// 	map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+    map.addChild(new YMapDefaultSchemeLayer());
+    map.addChild(new YMapDefaultFeaturesLayer());
 
-// 	map.geoObjects.add(myPlacemark);
-//     myPlacemark.balloon.open();
+    map.addChild(new YMapControls({position: 'right'}).addChild(new YMapZoomControl({})));
 
-// }
+    const myMarker = document.createElement('div');
+    myMarker.className = 'my-marker';
+    // myMarker.src = './location-pin.svg';
+
+    myMarker.onclick = () => map.update({location: {...LOCATION, duration: 400}});
+    map.addChild(new YMapMarker({coordinates: LOCATION.center}, myMarker));
+}
